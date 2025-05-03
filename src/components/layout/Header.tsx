f100daddaf65cc83c19ee70f72d8f076c5d69669
sayed-cmd <115411804+sayed-cmd@users.noreply.github.com>
2025-05-03 22:59:48 +0600

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
     <header className="sticky top-0 bg-white/95 backdrop-blur-sm shadow-sm z-50">
       <div className="container py-4 flex justify-between items-center">
         <a href="#" className="text-2xl font-bold text-primary">
-          Sayed's <span className="text-secondary">Portfolio</span>
+          💼 <span className="text-secondary"> Portfolio </span>
         </a>
 
         {/* Desktop Navigation */}
